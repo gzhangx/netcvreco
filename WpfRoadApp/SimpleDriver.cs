8895f98efecfd4807505b76e415b7a9c96006e3b
Gang Zhang <gzhangx@hotmail.com>
2018-09-25 22:04:59 -0400

    never fail

--- WpfRoadApp/SimpleDriver.cs
+++ WpfRoadApp/SimpleDriver.cs
@@ -44,26 +44,32 @@ namespace WpfRoadApp
         public void Drive(string data)
         {
             if (!sendCommand) return;
-            // Create a request for the URL.   
-            WebRequest request = WebRequest.Create(
-           $"{url}/{data}");
-            // If required by the server, set the credentials.  
-            request.Credentials = CredentialCache.DefaultCredentials;
-            // Get the response.  
-            WebResponse response = request.GetResponse();
-            // Display the status.  
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.  
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.  
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.  
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.  
-            Console.WriteLine(responseFromServer);
-            // Clean up the streams and the response.  
-            reader.Close();
-            response.Close();
+            try
+            {
+                // Create a request for the URL.   
+                WebRequest request = WebRequest.Create(
+               $"{url}/{data}");
+                // If required by the server, set the credentials.  
+                request.Credentials = CredentialCache.DefaultCredentials;
+                // Get the response.  
+                WebResponse response = request.GetResponse();
+                // Display the status.  
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                // Get the stream containing content returned by the server.  
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.  
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.  
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.  
+                Console.WriteLine(responseFromServer);
+                // Clean up the streams and the response.  
+                reader.Close();
+                response.Close();
+            } catch (Exception exc)
+            {
+                Console.WriteLine($"Driver Failure {exc.Message}");
+            }
         }
     }
 }
