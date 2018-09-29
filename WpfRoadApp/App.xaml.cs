ced01f8be3dd87d6d33f079f38fa20ec5d6f24d5
Gang Zhang <gzhangx@hotmail.com>
2018-09-29 12:59:32 -0400

    pre-start threads

--- WpfRoadApp/App.xaml.cs
+++ WpfRoadApp/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -15,7 +16,27 @@ namespace WpfRoadApp
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            
             log4net.Config.BasicConfigurator.Configure();
+            List<Task> tasks = new List<Task>();
+            for(int i = 0; i< 4;i++)
+            {
+                tasks.Add(ReadyTasks());
+            }
+            tasks.ForEach(t => t.Wait());
+            Console.WriteLine("All tasks ready");
+        }
+
+        static int debugTskCount = 0;
+        private Task ReadyTasks()
+        {
+            return Task.Run(() =>
+            {
+                int who = ++debugTskCount;
+                Console.WriteLine($"Started {who}");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Ending task {who}");
+            });
         }
     }
 }
