f16412de22c8bde2500733be89e0aac44a29c389
Gang Zhang <gzhangx@hotmail.com>
2018-10-06 23:59:03 -0400

    adding haar

---
+++ netCvLib/harrc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Drawing;
+
+namespace netCvLib
+{
+    public class GZHarC
+    {
+        CascadeClassifier haar;
+        public GZHarC()
+        {
+            haar = new CascadeClassifier("haarcascade_frontalface_alt2.xml");            
+        }
+
+        public Rectangle[] Detect(Mat m)
+        {
+            return haar.DetectMultiScale(m);
+        }
+    }
+}
