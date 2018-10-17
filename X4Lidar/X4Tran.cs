b74345c59ec6480ee1f3baf15ba6fe15712ac1f4
Gang Zhang <gzhangx@hotmail.com>
2018-10-16 22:31:58 -0400

    adding lidar package

---
+++ X4Lidar/X4Tran.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.veda.X4Lidar
+{
+    public interface IX4Tran
+    {
+        void Translate(byte[] data);
+    }
+    public class RadAndLen
+    {
+        public double Rad { get; private set; }
+        public int Len { get; private set; }
+        public RadAndLen(double ang, int len)
+        {
+            Rad = ang;
+            Len = len;
+        }
+        public int X { get
+            {
+                return (int)(Math.Cos(Rad) * Len);
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return (int)(Math.Sin(Rad) * Len);
+            }
+        }
+    }
+    public class X4Tran : IX4Tran
+    {
+        Action<RadAndLen> addAction;
+        Action<double> zeroAng;
+        public X4Tran(Action<RadAndLen> add, Action<double> ang)
+        {
+            addAction = add;
+            zeroAng = ang;
+        }
+        MemoryStream ms = new MemoryStream();
+        int count = 0;
+        public void Translate(byte[] data)
+        {
+            if (ms.Length == 0)
+            {
+                if (data.Length > 3 && data[0] == 0xaa && data[1] == 0x55)
+                {
+                    var lsn = ((uint)data[3]);
+                    int totalLen = 10 + (int)(lsn * 2);
+                    if (data.Length < totalLen)
+                    {
+                        ms.Write(data, 0, data.Length);
+                    }
+                    else if (data.Length > totalLen)
+                    {
+                        ms.Write(data, totalLen, data.Length - totalLen);
+                        var nd = new byte[totalLen];
+                        Array.Copy(data, nd, totalLen);
+                        DoTranslate(nd);
+                        var ndata = ms.ToArray();
+                        ms.SetLength(0);
+                        Translate(ndata);
+                    }
+                    else DoTranslate(data);
+                }
+            }else
+            {
+                ms.Write(data, 0, data.Length);
+                var ndata = ms.ToArray();
+                ms.SetLength(0);
+                Translate(ndata);
+            }
+        }
+        double curZeroAng = 0;
+        public void DoTranslate(byte[] data)
+        {
+            //if (data.Length > 2 && data[0] == 0xaa && data[1] == 0x55)
+            {
+                var fsa = getAngle(data, 4);
+                var lsa = getAngle(data, 6);
+                var lsn = ((uint)data[3]) - 1;
+                if (lsn == 0)
+                {                    
+                    count = 0;
+                    zeroAng(fsa);
+                    curZeroAng = fsa;                    
+                    return;
+                }
+                //Console.WriteLine($"{ lsn} fs { fsa} lsa { lsa}");
+                Func<uint,uint> getLen = start => {
+                    if (start+1 > data.Length)
+                    {
+                        Console.WriteLine($"bad start on data, start={start} len={data.Length} data={BitConverter.ToString(data)}");
+                    }
+                    var dstart = 10;
+                    return (data[start + dstart] | (((uint)data[start + dstart + 1]) << 8)) >> 2;
+                };
+                var lenFsa = getLen(0);
+                var lenLsa = getLen(lsn * 2);
+                var anglefsa = fsa + calculateCorr(lenFsa);
+                var anglelsa = lsa + calculateCorr(lenLsa);
+                var diffAng = (anglelsa - anglefsa);
+                //Console.WriteLine(data);
+                //Console.WriteLine(`Angle fsa ${anglefsa*180/Math.PI} ${anglelsa*180/Math.PI}`);
+                for (uint i = 0; i <= lsn; i++)
+                {
+                    var len = getLen(i * 2);
+                    //Console.WriteLine(`data ${toHex(data[i*2+dstart])} ${toHex(data[i*2+1+dstart])} len=${len} `);
+                    var ai = (diffAng / lsn * i) + anglefsa + calculateCorr(len);
+                    //Console.WriteLine(`len=${len} ang=${ai*180/Math.PI}`);
+                    //var x = Math.Cos(ai) * len;
+                    //var y = Math.Sin(ai) * len;
+
+                    if (len != 0)
+                    {
+                        //fs.appendFile('data.txt',`${ x},${ y}\r\n`,err => {
+                        //if (err) Console.WriteLine(err);
+                        addAction(new RadAndLen(ai, (int)len));
+                        count++;
+                    };
+                }
+            }
+        }        
+
+        double calculateCorr(uint dist)
+        {
+            if (dist == 0) return 0;
+            var corr = Math.Atan(21.8 * (155.3 - dist) / (155.3 * dist));
+            return corr;
+        }
+        double getAngle(byte[] data, int start)
+        {
+            return ((data[start] | (((uint)data[start + 1]) << 8)) / 128) * Math.PI / 180;
+        }
+    }
+}
---
+++ X4Lidar/scom.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace com.veda.X4Lidar
+{
+    class scom
+    {
+        protected SerialPort comm = new SerialPort();
+        protected Thread _thread;
+        protected bool threadStarted = false;
+        public scom()
+        {
+            //comm.ReadTimeout = 500;
+            //comm.WriteTimeout = 500;
+            comm.Parity = Parity.None;
+            comm.DataBits = 8;
+            comm.StopBits = StopBits.One;
+            //comm.WriteBufferSize = 2048;
+            //comm.ReadBufferSize = 2048;
+            comm.DataReceived += Comm_DataReceived;
+            comm.ErrorReceived += Comm_ErrorReceived;
+            comm.RtsEnable = false;
+            comm.BaudRate = 128000;
+            comm.PortName = "COM3";
+
+            comm = new SerialPort("COM3", 128000, Parity.None, 8, StopBits.One);
+            comm.ReadTimeout = 0;
+            comm.RtsEnable = false;
+        }
+
+        private void Comm_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            Console.WriteLine("errro!");
+        }
+
+        private void Comm_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine("data receive");
+        }
+
+        public void Open()
+        {                       
+            comm.Open();
+        }
+
+        public void Close()
+        {
+            threadStarted = false;
+            comm.Close();
+            _thread.Join();
+            _thread = null;
+        }
+
+
+        public void Start()
+        {
+            //Write(new byte[] { 0xA5, 0x90 });
+            Write(new byte[] { 0xA5, 0x60 });
+            threadStarted = true;
+            if (_thread != null) return;            
+            _thread = new Thread(() =>
+            {
+                var buf = new byte[2048];
+                while (threadStarted)
+                {
+                    try
+                    {
+                        int blen = comm.Read(buf, 0, buf.Length);
+                        if (blen < 0) break;
+                        Console.WriteLine($"Got item {blen} {BitConverter.ToString(buf, 0, blen)}");
+                    } catch (TimeoutException)
+                    {
+                    }
+                }
+                Console.WriteLine("thread done");
+            });
+            _thread.Start();
+
+
+
+        }
+
+        public void Info()
+        {
+            Write(new byte[] { 0xA5, 0x90 });
+        }
+        public void Stop()
+        {
+            Write(new byte[] { 0xA5, 0x65 });
+        }
+        protected void Write(byte[] buf)
+        {
+            comm.Write(buf, 0, buf.Length);
+            comm.BaseStream.Flush();
+        }
+    }
+}
