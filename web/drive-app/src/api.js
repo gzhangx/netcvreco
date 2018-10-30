3a2e318fe16e1055c5c74f4acae4332c2b5cd0e3
gang zhang <gang.zhang@pps.io>
2018-10-29 23:32:48 -0400

    fixapi

--- web/drive-app/src/api.js
+++ web/drive-app/src/api.js
@@ -2,7 +2,7 @@ import request from 'superagent';
 
 export default {
   cmdReq: url=>{
-      return request.get(`http://localhost:8001/${url}`).send().then(r=> {
+      return request.get(`/${url}`).send().then(r=> {
           console.log('request returns');
           console.log(r);
       });
