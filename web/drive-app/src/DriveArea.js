7259dd611a13ede24ce425a811d95a8f85efa0c6
gang zhang <gang.zhang@pps.io>
2018-11-05 20:24:19 -0500

    adding throttle

--- web/drive-app/src/DriveArea.js
+++ web/drive-app/src/DriveArea.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import api from './api';
+import debounce from 'lodash/debounce';
 
 class DriveArea extends Component{
     constructor(props) {
         super(props);
 
-        this.state = { x: 0, y: 0 };
+        this.state = { x: 100, y: 0 };
+        this.sendCmd = this.sendCmd.bind(this);
     }
 
     _onMouseMove(e) {
@@ -14,20 +16,27 @@ class DriveArea extends Component{
         //200->180
         //const boundingRect = this.refs.drawArea.getBoundingClientRect(); //left top
         //e.screenX - boundingRect.left, y: e.screenY - boundingRect.top
-        let x = parseInt((e.nativeEvent.offsetX -5)/10)*10;
-        let y = 100 - e.nativeEvent.offsetY;
-        if (y < 0) y = 0;
-        y = parseInt(y/20);
-        this.setState({ x, y });
+        const x = e.nativeEvent.offsetX;
+        const y = e.nativeEvent.offsetY;
+        const me = this;
+        debounce(()=>{
+            me.setState({ x, y });
+            me.sendCmd(x,y);
+        }, 100)();
+    }
 
+    sendCmd(xx,yy) {
+        let y = parseInt((100-yy)/20);
+        let x = (xx/200*20)+90;
         api.cmdReq(`api/r/${x}`);
         api.cmdReq(`api/d/${y}`);
+        this.setState({displayX: x, displayY:y});
     }
 
     render() {
-        const { x, y } = this.state;
+        const { x, y, displayX, displayY } = this.state;
         return <div ref="drawArea" onMouseMove={this._onMouseMove.bind(this)} className="App-work-area">
-            <p>Mouse coordinates: { x },{ y }</p>
+            <p>Mouse coordinates: { x },{ y } ({ displayX}, {displayY})</p>
             <svg>
                 <line
                     x1={100}
@@ -38,7 +47,7 @@ class DriveArea extends Component{
                         stroke: '#458232',
                         strokeWidth: '3px',
                     }}
-                ></line>
+                />
             </svg>
         </div>;
     }
