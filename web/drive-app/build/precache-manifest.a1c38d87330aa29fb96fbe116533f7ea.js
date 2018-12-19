fe0e8f7a89453a4dd6ad58adaa6c4d9b19f3266c
gang zhang <gang.zhang@pps.io>
2018-12-19 12:57:33 -0500

    new build

---
+++ web/drive-app/build/precache-manifest.a1c38d87330aa29fb96fbe116533f7ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd78f42d4f8aa493c8d",
+    "url": "/static/css/main.43a73c60.chunk.css"
+  },
+  {
+    "revision": "dcd78f42d4f8aa493c8d",
+    "url": "/static/js/main.dcd78f42.chunk.js"
+  },
+  {
+    "revision": "f774b29938a8442ff36c",
+    "url": "/static/js/1.f774b299.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25f9a51ce84e7f796f5503e9f32b996d",
+    "url": "/index.html"
+  }
+];
