f97c068fcea67445ec3edeb0fe0b8b238d414c87
Gang Zhang <gzhang@hotmail.com>
2019-05-11 00:10:48 -0400

    test

--- tests/MatrixTest/Program.cs
+++ tests/MatrixTest/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
 using System.IO;
+using com.veda.LinearAlg;
 
 namespace MatrixTest
 {
@@ -198,89 +199,89 @@ namespace MatrixTest
         }
     }
 
-    public class GMatrix
-    {
-        public double[][] storage { get; protected set; }
-        public GMatrix(int r, int c)
-        {
-            rows = r;
-            cols = c;
-            init();
-        }
-        public GMatrix(double[,] v)
-        {            
-            rows = v.GetLength(0);
-            cols = v.GetLength(1);
-            init();
-            for (var r = 0; r < rows; r++)
-            {
-                for (var c = 0; c<cols;c++)
-                {
-                    storage[r][c] = v[r, c];
-                }
-            }
-        }
-        public void init()
-        {
-            storage = new double[rows][];
-            for (var i = 0; i < rows; i++)
-            {
-                storage[i] = new double[cols];
-            }
-        }
-        public GMatrix tranpose()
-        {
-            var r = rows;
-            var c = cols;
-            var newStorage = new GMatrix(c,r);
-            for (var i = 0; i < r; i++)
-            {
-                for(var j = 0; j < c;j++)
-                {
-                    newStorage.storage[j][i] = storage[i][j];
-                }
-            }
-            return newStorage;
-        }
-        public int cols { get; protected set; }
-        public int rows { get; protected set; }
+    //public class GMatrix
+    //{
+    //    public double[][] storage { get; protected set; }
+    //    public GMatrix(int r, int c)
+    //    {
+    //        rows = r;
+    //        cols = c;
+    //        init();
+    //    }
+    //    public GMatrix(double[,] v)
+    //    {            
+    //        rows = v.GetLength(0);
+    //        cols = v.GetLength(1);
+    //        init();
+    //        for (var r = 0; r < rows; r++)
+    //        {
+    //            for (var c = 0; c<cols;c++)
+    //            {
+    //                storage[r][c] = v[r, c];
+    //            }
+    //        }
+    //    }
+    //    public void init()
+    //    {
+    //        storage = new double[rows][];
+    //        for (var i = 0; i < rows; i++)
+    //        {
+    //            storage[i] = new double[cols];
+    //        }
+    //    }
+    //    public GMatrix tranpose()
+    //    {
+    //        var r = rows;
+    //        var c = cols;
+    //        var newStorage = new GMatrix(c,r);
+    //        for (var i = 0; i < r; i++)
+    //        {
+    //            for(var j = 0; j < c;j++)
+    //            {
+    //                newStorage.storage[j][i] = storage[i][j];
+    //            }
+    //        }
+    //        return newStorage;
+    //    }
+    //    public int cols { get; protected set; }
+    //    public int rows { get; protected set; }
 
-        public GMatrix cross(GMatrix m)
-        {
-            var r = rows;
-            var mc = m.cols;
-            if (r != mc) throw new InvalidOperationException($"Cross: row {r} and col {mc} must equal");
-            if (cols != m.rows) throw new InvalidOperationException($"Cross: col {cols} and row {m.rows} must equal");
-            var newStorage = new double[r, mc];
-            var c = cols;
-            for (var i = 0; i < r; i++)
-            {
-                for (var j = 0; j < mc; j++)
-                {
-                    double total = 0;
-                    for (var k = 0; k < c; k++)
-                    {
-                        total += storage[i][k] * m.storage[k][j];
-                    }
-                    newStorage[i, j] = total;
-                }
-            }
-            return new GMatrix(newStorage);
-        }
+    //    public GMatrix cross(GMatrix m)
+    //    {
+    //        var r = rows;
+    //        var mc = m.cols;
+    //        if (r != mc) throw new InvalidOperationException($"Cross: row {r} and col {mc} must equal");
+    //        if (cols != m.rows) throw new InvalidOperationException($"Cross: col {cols} and row {m.rows} must equal");
+    //        var newStorage = new double[r, mc];
+    //        var c = cols;
+    //        for (var i = 0; i < r; i++)
+    //        {
+    //            for (var j = 0; j < mc; j++)
+    //            {
+    //                double total = 0;
+    //                for (var k = 0; k < c; k++)
+    //                {
+    //                    total += storage[i][k] * m.storage[k][j];
+    //                }
+    //                newStorage[i, j] = total;
+    //            }
+    //        }
+    //        return new GMatrix(newStorage);
+    //    }
 
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            for (var i = 0; i < rows; i++)
-            {
-                for (var j = 0; j < cols;j++)
-                {
-                    if (j > 0) sb.Append(",");
-                    sb.Append(storage[i][j].ToString("0.00000"));                    
-                }
-                sb.Append("\n");
-            }
-            return sb.ToString();
-        }
-    }
+    //    public override string ToString()
+    //    {
+    //        var sb = new StringBuilder();
+    //        for (var i = 0; i < rows; i++)
+    //        {
+    //            for (var j = 0; j < cols;j++)
+    //            {
+    //                if (j > 0) sb.Append(",");
+    //                sb.Append(storage[i][j].ToString("0.00000"));                    
+    //            }
+    //            sb.Append("\n");
+    //        }
+    //        return sb.ToString();
+    //    }
+    //}
 }
