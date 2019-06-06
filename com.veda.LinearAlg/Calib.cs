550fce2fe97f85443524443ea5ec33e24f4c7657
Gang Zhang <gzhang@hotmail.com>
2019-06-06 00:09:46 -0400

    refactor fhat calc

--- com.veda.LinearAlg/Calib.cs
+++ com.veda.LinearAlg/Calib.cs
@@ -15,29 +15,8 @@ namespace com.veda.LinearAlg
     }
     public class Calib
     {
-        public static GMatrix Calc(PointFloat[] points1, PointFloat[] points2)
+        public static GMatrix SolveSvd(GMatrix m1)
         {
-            var m1 = new GMatrix(points1.Length, 9);
-            for (var i = 0; i < points1.Length; i++)
-            {
-                var pp1 = points1[i];
-                var pp2 = points2[i];
-                var y1 = pp1.X;
-                var y2 = pp1.Y;
-                var p1 = pp2.X;
-                var p2 = pp2.Y;
-                var cur = m1.storage[i];
-                cur[0] = y1 * p1;
-                cur[1] = p1 * y2;
-                cur[2] = p1;
-                cur[3] = p2 * y1;
-                cur[4] = p2 * y2;
-                cur[5] = p2;
-                cur[6] = y1;
-                cur[7] = y2;
-                cur[8] = 1;
-            }
-
             double max = 1;
             double min = -1;
             for (var i = 0; i < m1.rows; i++)
@@ -68,6 +47,32 @@ namespace com.veda.LinearAlg
                     Fhat.storage[i][j] = svdA.Vt.storage[at++][8];
                 }
             }
+            return Fhat;
+        }
+        public static GMatrix Calc(PointFloat[] points1, PointFloat[] points2)
+        {
+            var m1 = new GMatrix(points1.Length, 9);
+            for (var i = 0; i < points1.Length; i++)
+            {
+                var pp1 = points1[i];
+                var pp2 = points2[i];
+                var y1 = pp1.X;
+                var y2 = pp1.Y;
+                var p1 = pp2.X;
+                var p2 = pp2.Y;
+                var cur = m1.storage[i];
+                cur[0] = y1 * p1;
+                cur[1] = p1 * y2;
+                cur[2] = p1;
+                cur[3] = p2 * y1;
+                cur[4] = p2 * y2;
+                cur[5] = p2;
+                cur[6] = y1;
+                cur[7] = y2;
+                cur[8] = 1;
+            }
+            
+            var Fhat = SolveSvd(m1);
 
             var FhatSvd = JacobSvd.JacobiSVD(Fhat);
             var d = FhatSvd.getWMat();
